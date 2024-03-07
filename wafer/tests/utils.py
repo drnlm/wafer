72a924681eea19f60166b2d1a041afe8e3ad1562
Neil Muller <drnlmuller@gmail.com>
2024-03-07 15:15:18 +0200

    Ludicrious non-speed

--- wafer/tests/utils.py
+++ wafer/tests/utils.py
@@ -54,7 +54,7 @@ def mock_avatar_url(self):
 
 
 # Time to wait for elements to appear - single constant so we can easily tune it if needed
-SELENIUM_WAIT_TIME = 40
+SELENIUM_WAIT_TIME = 120
 
 
 @tag('selenium')
